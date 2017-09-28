d17c58c21502bd3e34e6e5023ee9822e8d5b61de
Nurul Amin <Nurul15@hotmail.co.uk>
2017-09-28 13:36:51 +0100

    Added smooth scrolling to top

--- js/blog.js
+++ js/blog.js
@@ -40,3 +40,26 @@ $(document).ready(function(){
     $('html, body').animate({scrollTop: $('a[name='+getAName[1]+']').offset().top},1000);
   })
 })
+
+// Button to scroll to top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollBtn").style.display = "block";    
+  }
+  else {
+    document.getElementById("scrollBtn").style.display = "none";
+  }
+}
+
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+$("button").click(function() {
+  $('html,body').animate({
+    scrollTop: $(".masthead").offset().top},
+    'slow');
+});
